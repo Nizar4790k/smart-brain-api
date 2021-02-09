9bafacc3200df2e6e834e42538e2ed698c452065
Nizar <nizar4790k@gmail.com>
2021-02-09 19:18:48 -0400

    image and profile endpoinds are now available

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.json());
 
 const database = {
@@ -15,16 +16,16 @@ const database = {
             password:'cookies',
             entries:0,
             joined:new Date()
-        },
-        {
-            id:'124',
-            name:'Sally',
-            email:'sally@gmail.com',
-            password:'bananas',
-            entries:0,
-            joined:new Date()
+    },
+    {
+        id:'124',
+        name:'Sally',
+        email:'sally@gmail.com',
+        password:'bananas',
+        entries:0,
+        joined:new Date()
 
-        }
+    }
 
 
     ]
@@ -63,6 +64,48 @@ app.post('/register',(req,res)=>{
 
     res.json(database.users[database.users.length-1])
 })
+
+app.get('/profile/:id',(req,res)=>{
+    const { id } = req.params;
+    let found = false;
+    
+
+
+    database.users.forEach(user =>{
+        
+      
+        if(user.id ===id){
+        
+            found = true;
+            return res.json(user);
+        }
+
+        if(!found){  
+            res.status(400).json('not found');
+        }
+    })
+})
+
+app.post('/image',(req,res)=>{
+    
+    const { id } = req.body;
+    let found = false;
+    
+    database.users.forEach(user =>{
+      
+        if(user.id === id){
+        
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+
+        if(!found){  
+            res.status(400).json('not found');
+        }
+    })
+
+});
 
 app.listen(3000,()=>{
     console.log("app is runing on port 3000");
