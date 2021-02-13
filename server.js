d923c1479caa98c92962ddba572fd19efed8d117
Nizar <nizar4790k@gmail.com>
2021-02-13 07:59:50 -0400

    entries are now updated in the database

--- server.js
+++ server.js
@@ -113,23 +113,15 @@ app.get('/profile/:id',(req,res)=>{
 app.put('/image',(req,res)=>{
     
     const { id } = req.body;
-    let found = false;
-    
-    database.users.forEach(user =>{
-      
-        
-
-        if(user.id === id){
-        
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
-
-        if(!found){  
-            res.status(400).json('not found');
-        }
+    db('users').where('id','=',id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries=>{
+        res.json(entries[0])
     })
+    .catch(err=>{
+        console.log(err);
+        res.status(400).json('unable to get entries')});
 
 });
 
